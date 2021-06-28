aa657c82acdeb08ae06486035c551adf910646d2
Hai-Nam Nguyen <hainam@jcisio.com>
2021-06-28 10:24:48 +0200

    Add tests

--- src/SecurityChecker.php
+++ src/SecurityChecker.php
@@ -44,6 +44,8 @@ class SecurityChecker
      *
      * @param string $composer_lock
      *   The composer.lock file to check.
+     * @param bool $exclude_dev
+     *   Only check for production dependencies.
      *
      * @return array
      *   An array with the status and the possibles vulnerabilities founded.
@@ -52,7 +54,7 @@ class SecurityChecker
      *
      * @throws \Exception
      */
-    public function checkComposer($composer_lock)
+    public function checkComposer($composer_lock, bool $exclude_dev = false)
     {
         $vulnerabilities = [];
         $status = 'ok';
@@ -62,7 +64,8 @@ class SecurityChecker
             $security_advisories_composer_json = $this->fetchAdvisoryComposerJson($url);
             $updates = $this->calculateSecurityUpdates(
                 $composer_lock_data,
-                $security_advisories_composer_json
+                $security_advisories_composer_json,
+                $exclude_dev
             );
             if (!empty($updates)) {
                 $vulnerabilities += $updates;
@@ -113,6 +116,8 @@ class SecurityChecker
      *   The contents of a composer.lock file.
      * @param array $security_advisories_composer_json
      *   The composer.json array from the security-advisory.
+     * @param bool $exclude_dev
+     *   Only check for production dependencies.
      *
      * @return array
      *   Security updates availables, keyed by package name,
@@ -121,10 +126,10 @@ class SecurityChecker
      *   - version
      *   - links.
      */
-    protected function calculateSecurityUpdates(array $composer_lock_data, array $security_advisories_composer_json, bool $excludeDev = false)
+    protected function calculateSecurityUpdates(array $composer_lock_data, array $security_advisories_composer_json, bool $exclude_dev = false)
     {
-        if ($excludeDev) {
-            $packages = $composer_lock_data['packages-dev'];
+        if ($exclude_dev) {
+            $packages = $composer_lock_data['packages'];
         }
         else {
             $packages = array_merge(
--- tests/src/SecurityCheckerTest.php
+++ tests/src/SecurityCheckerTest.php
@@ -20,6 +20,8 @@ class SecurityCheckerTest extends TestCase
      *   Url of composer.json containing security advisories.
      * @param string $lock
      *   Url of composer.lock to check.
+     * @param bool $exclude_dev
+     *   Exclude_dev option.
      * @param string $expected_status
      *   Expected status of test.
      *
@@ -27,14 +29,14 @@ class SecurityCheckerTest extends TestCase
      *
      * @throws \Exception
      */
-    public function testChecker($url, $lock, $expected_status)
+    public function testChecker($url, $lock, $exclude_dev, $expected_status)
     {
         $composer_lock = file_get_contents($lock);
         $this->assertIsString($composer_lock);
 
         if (is_string($composer_lock)) {
             $checker = new SecurityChecker([$url]);
-            $data = $checker->checkComposer($composer_lock);
+            $data = $checker->checkComposer($composer_lock, $exclude_dev);
 
             $this->assertArrayHasKey('status', $data);
 
@@ -53,15 +55,42 @@ class SecurityCheckerTest extends TestCase
      */
     public function dataProvider()
     {
+        $data_dir = __DIR__ . '/../data';
         return [
-            [
-                __DIR__ . '/../composer.json',
-                __DIR__ . '/../data/drupal8.8.0/composer.lock',
+            'drupal-roave' => [
+                $data_dir . '/composer-roave.json',
+                $data_dir . '/drupal8.8.0/composer.lock',
+                false,
                 'vulnerable',
             ],
-            [
-                __DIR__ . '/../composer.json',
-                __DIR__ . '/../data/symfony4.3.8/composer.lock',
+            'symfony-roave' => [
+                $data_dir . '/composer-roave.json',
+                $data_dir . '/symfony4.3.8/composer.lock',
+                false,
+                'ok',
+            ],
+            'dev-vulnerable-roave' => [
+                $data_dir . '/composer-roave.json',
+                $data_dir . '/dev-vulnerable/composer.lock',
+                false,
+                'ok',
+            ],
+            'dev-vulnerable-roave-nodev' => [
+                $data_dir . '/composer-roave.json',
+                $data_dir . '/dev-vulnerable/composer.lock',
+                true,
+                'ok',
+            ],
+            'dev-vulnerable-drupal' => [
+                $data_dir . '/composer-drupal.json',
+                $data_dir . '/dev-vulnerable/composer.lock',
+                false,
+                'vulnerable',
+            ],
+            'dev-vulnerable-drupal-nodev' => [
+                $data_dir . '/composer-drupal.json',
+                $data_dir . '/dev-vulnerable/composer.lock',
+                true,
                 'ok',
             ],
         ];
